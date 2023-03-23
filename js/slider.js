ca486a40ac6319ef3903596c998182362169acae
Andrefg29 <andre_f_giacomelli@estudante.sesisenai.org.br>
2023-03-23 11:34:56 -0300

    show

--- js/slider.js
+++ js/slider.js
@@ -1,51 +1,23 @@
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
-let currentSlide = 1;
-
-//Javascript para Slider Manual
-
-var manualNav = function(manual){
-    slides.forEach((slide) => {
-    slide.classList.remove('active');
-
-    btns.forEach((btn) => {
-        btn.classList.remove('active');
-    });
-    });
-
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
-
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-    manualNav(i);
-    currentSlide = i;
-    });
-});
-
-var repeat = function(activeClass){
-    let active = document.getElementsByClassName('active');
-    let i = 1;
-
-    var repeater = () => {
-        setTimeout(function(){
-            [...active].forEach((activeSlide) => {
-                activeSlide.classList.remove('active');
-            });
-            slides[i].classList.add('active');
-            btns[i].classList.add('active');
-            i++;
-
-            if(slides.length == i){
-                i = 0;
-            }
-            if(i >= slides.length){
-                return;
-            }
-            repeater();
-        }, 10000);
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    repeater();
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
-repeat();
